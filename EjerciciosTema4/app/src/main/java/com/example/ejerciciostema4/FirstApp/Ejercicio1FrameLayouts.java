fee6783b6370814a7a916cfd660f735518653e38
Álvaro Gómez Rueda <alvaroogr22@gmail.com>
2024-01-08 11:16:23 +0100

    Ejerciciostema4

---
+++ EjerciciosTema4/app/src/main/java/com/example/ejerciciostema4/FirstApp/Ejercicio1FrameLayouts.java
@@ -0,0 +1,4 @@
+package com.example.ejerciciostema4.FirstApp;
+
+public class Ejercicio1FrameLayouts {
+}
---
+++ EjerciciosTema4/app/src/main/java/com/example/ejerciciostema4/FirstApp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ejerciciostema4.FirstApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ejerciciostema4.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
